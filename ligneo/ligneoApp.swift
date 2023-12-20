3dd772c9cbdec02d5767afec0f259941d9db643b
Zakarya Tolba <zakarya.tolba@ynov.com>
2023-12-20 21:19:45 +0100

    Initial Commit

---
+++ ligneo/ligneoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ligneoApp.swift
+//  ligneo
+//
+//  Created by Zakarya TOLBA on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ligneoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ligneoUITests/ligneoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ligneoUITestsLaunchTests.swift
+//  ligneoUITests
+//
+//  Created by Zakarya TOLBA on 20/12/2023.
+//
+
+import XCTest
+
+final class ligneoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
